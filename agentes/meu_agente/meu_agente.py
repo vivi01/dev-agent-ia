080de2cc0f11951a3899d9531c16df07bcae3951
Viviane dos Santos Cruz Pereira <vivianecruz01@hotmail.com>
2026-06-25 18:23:16 -0300

    novo agente

---
+++ agentes/meu_agente/meu_agente.py
@@ -0,0 +1,3 @@
+from google.adk.agents import Agent
+
+root_agent = Agent(name="Meu Agente", description="Um agente de exemplo usando a Google ADK.")
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from dev-agents-ia!")
-
-
-if __name__ == "__main__":
-    main()
